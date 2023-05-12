db43b2e6cf1916dda7481f992b327fac20d4e973
MicheleStaffiere <michelestaffiere1@gmail.com>
2023-05-11 23:42:37 -0400

    carousel now fully working, needs to be styled.

--- scripts/app.js
+++
@@ -1,6 +0,0 @@
-import carouselHandling,{viewPortHandling} from "./carousel-Category-Banners.js";
-let trackNum = 0;
-carouselHandling();
-viewPortHandling();
-leftBtn.addEventListener('click', () =>{carouselHandling(`left`)});
-rightBtn.addEventListener('click', () =>{carouselHandling(`right`)});
